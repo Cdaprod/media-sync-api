aa0e68131fc57af4ca94637c1894b77495c4b08a
David Cannan <104325852+Cdaprod@users.noreply.github.com>
2026-04-26 09:20:46 -0400

    Add rewrites for media and API endpoints in config

--- docker/packages/Explorer/next.config.js
+++ docker/packages/Explorer/next.config.js
@@ -1,6 +1,34 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+      return [
+        {
+          source: '/media/:path*',
+          destination: 'http://host.docker.internal:8787/media/:path*',
+        },
+        {
+          source: '/thumbnails/:path*',
+          destination: 'http://host.docker.internal:8787/thumbnails/:path*',
+        },
+        {
+          source: '/api/:path*',
+          destination: 'http://host.docker.internal:8787/api/:path*',
+        },
+        {
+          source: '/connect/:path*',
+          destination: 'http://host.docker.internal:8787/connect/:path*',
+        },
+        {
+          source: '/debug/:path*',
+          destination: 'http://host.docker.internal:8787/debug/:path*',
+        },
+        {
+          source: '/public/:path*',
+          destination: 'http://host.docker.internal:8787/public/:path*',
+        },
+      ];
+    },
   webpack: (config, { dev }) => {
     if (dev) {
       config.watchOptions = {
